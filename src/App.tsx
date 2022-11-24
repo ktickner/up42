0eecfab4c825945436e5197dcea2fad3b3732cf9
Kyle Tickner <ktickner.91@gmail.com>
2022-11-24 15:40:21 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
